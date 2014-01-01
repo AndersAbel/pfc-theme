bf1048f688f2234e73597c611d6464512543d3fb
Anders Abel <anders@abel.nu>
2014-01-01 18:24:13 +0100

    Post listing finished in first version.

--- index.php
+++ index.php
@@ -3,8 +3,22 @@
 <div id="primary" class="col-md-8 post-list">
     <?php if( have_posts() ) : ?>
     <?php while( have_posts() ): the_post(); ?>
-        <a href="<?php the_permalink(); ?>"><h1 class="entry-title"><?php the_title(); ?></h1></a>
+    <article>
+        <header class="entry-header">
+            <a href="<?php the_permalink(); ?>"><h1 class="entry-title"><?php the_title(); ?></h1></a>
+            <div class="entry-meta small text-right">
+                Posted on <?php the_date(); ?> by <?php the_author(); ?>
+            </div>
+        </header>
         <?php the_content('Continue Reading &rarr;'); ?>
+        <footer class="entry-meta">
+            Posted in 
+            <?php 
+                echo get_the_category_list(', ');
+                echo get_the_tag_list(' | Tagged ', ', '); 
+            ?>
+        </footer>
+    </article>
     <?php endwhile; ?>
     <?php endif; ?>
 </div>
