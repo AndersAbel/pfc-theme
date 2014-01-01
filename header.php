3cb5107710ae85208f359a1927c5e61daa38571e
Anders Abel <anders@abel.nu>
2014-01-01 20:43:56 +0100

    Added social media buttons to nav bar

--- header.php
+++ header.php
@@ -60,6 +60,14 @@
                                 'walker' => new Bootstrap_Walker_Nav_Menu() )
                                 );
                 ?>
+                <div class="navbar-right">
+                    <a class="navbar-custom-button" href="https://plus.google.com/+AndersAbel" rel="publisher" target="_blank">
+                        <img src="//ssl.gstatic.com/images/icons/gplus-32.png" alt="Google+" />
+                    </a>
+                    <a class="navbar-custom-button email-button" href="mailto:anders@abel.nu">&nbsp;</a>
+                    <a class="navbar-custom-button twitter-follow-button" href="https://twitter.com/anders_abel" target="_blank">&nbsp;</a>
+                    <a class="navbar-custom-button rss-button" href="<?php bloginfo('rss2_url'); ?>">&nbsp;</a>
+                </div>
             </div>
         </nav>
 
