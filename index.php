c75493c59c75a5f19112edd1271adb52d2ab4c45
Anders Abel <anders@abel.nu>
2014-01-02 14:00:43 +0100

    Added navigation on single post view.

--- index.php
+++ index.php
@@ -1,12 +1,12 @@
 <?php get_header(); ?>
 
 <div id="primary" class="col-md-8 <?php echo is_single() ? '' : 'post-list' ?>">
-    <?php if( have_posts() ){
-        while( have_posts() ){
+    <?php while( have_posts() ) :
             the_post();  
+            get_template_part("pager");
             get_template_part("content");
-        } 
-    } ?>
+            get_template_part("pager");
+          endwhile; ?>
 </div>
 <?php get_sidebar(); ?>
 <?php get_footer(); ?>
