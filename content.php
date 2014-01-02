433d0a6c85442572ec51291b11b07a08600d7c1e
Anders Abel <anders@abel.nu>
2014-01-02 12:13:20 +0100

    Moved content rendering to separate files for reuse.

---
+++ content.php
@@ -0,0 +1,16 @@
+<article>
+    <header class="entry-header">
+        <a href="<?php the_permalink(); ?>"><h1 class="entry-title"><?php the_title(); ?></h1></a>
+        <div class="entry-meta small text-right">
+            Posted on <?php the_date(); ?> by <?php the_author(); ?>
+        </div>
+    </header>
+    <?php the_content('Continue Reading &rarr;'); ?>
+    <footer class="entry-meta">
+        Posted in 
+        <?php 
+            echo get_the_category_list(', ');
+            echo get_the_tag_list(' | Tagged ', ', '); 
+        ?>
+    </footer>
+</article>
--- index.php
+++ index.php
@@ -1,26 +1,12 @@
 <?php get_header(); ?>
 
 <div id="primary" class="col-md-8 post-list">
-    <?php if( have_posts() ) : ?>
-    <?php while( have_posts() ): the_post(); ?>
-    <article>
-        <header class="entry-header">
-            <a href="<?php the_permalink(); ?>"><h1 class="entry-title"><?php the_title(); ?></h1></a>
-            <div class="entry-meta small text-right">
-                Posted on <?php the_date(); ?> by <?php the_author(); ?>
-            </div>
-        </header>
-        <?php the_content('Continue Reading &rarr;'); ?>
-        <footer class="entry-meta">
-            Posted in 
-            <?php 
-                echo get_the_category_list(', ');
-                echo get_the_tag_list(' | Tagged ', ', '); 
-            ?>
-        </footer>
-    </article>
-    <?php endwhile; ?>
-    <?php endif; ?>
+    <?php if( have_posts() ){
+        while( have_posts() ){
+            the_post();  
+            get_template_part("content");
+        } 
+    } ?>
 </div>
 <?php get_sidebar(); ?>
 <?php get_footer(); ?>
