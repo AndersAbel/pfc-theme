b17993e5cd9c48c37ff982dccfb7cd3d97d0f4a4
Anders Abel <anders@abel.nu>
2014-01-02 13:29:05 +0100

    Some styling and added G+ and twitter buttons

--- content.php
+++ content.php
@@ -1,8 +1,16 @@
 <article>
     <header class="entry-header">
         <a href="<?php the_permalink(); ?>"><h1 class="entry-title"><?php the_title(); ?></h1></a>
-        <div class="entry-meta small text-right">
-            Posted on <?php the_date(); ?> by <?php the_author(); ?>
+        <div class="row share-buttons">
+            <div class="share-buttons col-sm-6">
+                <?php if (is_single() ): ?>
+                    <a href="https://twitter.com/share" class="twitter-share-button" data-via="anders_abel">Tweet</a>
+                    <div class="g-plus" data-action="share" data-annotation="bubble"></div>
+                <?php endif; ?>
+            </div>
+            <div class="entry-meta small text-right col-sm-6">
+                Posted on <?php the_date(); ?> by <?php the_author(); ?>
+            </div>
         </div>
     </header>
     <?php the_content('Continue Reading &rarr;'); ?>
@@ -11,7 +19,7 @@
             Posted in 
             <?php 
                 echo get_the_category_list(', ');
-                echo get_the_tag_list(' | Tagged ', ', '); 
+                echo get_the_tag_list(' | Tagged ', ', ');
             ?>
         </footer>
     <?php endif; ?>
--- footer.php
+++ footer.php
@@ -16,6 +16,18 @@
     <script src="//ajax.googleapis.com/ajax/libs/jquery/1.10.2/jquery.min.js"></script>
     <!-- Include all compiled plugins (below), or include individual files as needed -->
     <script src="//netdna.bootstrapcdn.com/bootstrap/3.0.3/js/bootstrap.min.js"></script>
+    <!-- Support for twitter. -->
+    <script>!function(d,s,id){var js,fjs=d.getElementsByTagName(s)[0],p=/^http:/.test(d.location)?'http':'https';if(!d.getElementById(id)){js=d.createElement(s);js.id=id;js.src=p+"://platform.twitter.com/widgets.js";fjs.parentNode.insertBefore(js,fjs);}}(document,"script","twitter-wjs");</script>    
+    <!-- Support for Google+ -->
+    <script type="text/javascript">
+      window.___gcfg = {lang: 'en-GB'};
+
+      (function() {
+        var po = document.createElement('script'); po.type = 'text/javascript'; po.async = true;
+        po.src = 'https://apis.google.com/js/platform.js';
+        var s = document.getElementsByTagName('script')[0]; s.parentNode.insertBefore(po, s);
+      })();
+    </script>
     <?php wp_footer(); ?>
   </body>
 </html>
