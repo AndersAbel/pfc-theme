23bc00326efd811dd906fdd8b8d05cb222ae07cc
Anders Abel <anders@abel.nu>
2014-01-07 23:38:15 +0100

    Added ads above post and in blog post listing.

--- index.php
+++ index.php
@@ -1,6 +1,16 @@
 <?php get_header(); ?>
 
 <div id="primary" class="col-md-8 <?php echo is_single() ? '' : 'post-list' ?>">
+    <!-- Above Blog Post - Responsive -->
+    <ins class="adsbygoogle"
+         style="display:block"
+         data-ad-client="ca-pub-8682707145421648"
+         data-ad-slot="4616048858"
+         data-ad-format="auto"></ins>
+    <script>
+    (adsbygoogle = window.adsbygoogle || []).push({});
+    </script>
+
     <?php if ( !is_single() && get_previous_posts_link() ): ?>
         <ul class="pager">
             <li class="previous"><?php next_posts_link('&larr; Older Posts'); ?></li>
@@ -8,12 +18,25 @@
         </ul>
     <?php endif;?>
 
-    <?php while( have_posts() ) {
+    <?php for($i = 0; have_posts(); $i++ ) :
             the_post();  
             get_template_part('pager');
             get_template_part('content');
             get_template_part('pager');
-          }
+            if($i == 2): ?>
+                <!-- Below Post - Responsive -->
+                <ins class="adsbygoogle"
+                     style="display:block"
+                     data-ad-client="ca-pub-8682707145421648"
+                     data-ad-slot="9185849256"
+                     data-ad-format="auto"></ins>
+                <script>
+                (adsbygoogle = window.adsbygoogle || []).push({});
+                </script>                
+    <?php
+                
+            endif;
+          endfor;
 
         if(! is_single() ) : ?>
         <ul class="pager">
