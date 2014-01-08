563c7d8c30403432043c8bf3fcd68d06664543d8
Anders Abel <anders@abel.nu>
2014-01-08 15:56:44 +0100

    Added a 404 page

---
+++ 404.php
@@ -0,0 +1,12 @@
+<?php get_header(); ?>
+<div id="primary" class="col-md-8">
+    <h1>Who should be embarrassed?</h1>
+
+    <p class="lead">This is somewhat embarrassing. There is no page at this URL. If you got here by clicking 
+        a link somewhere on the site, then it's embarrasing for me. If you got here by typing an incorrect
+        link into the browser, then it's embarrasing for someone close to your keyboard.
+    </p>
+
+    </div>
+<?php get_sidebar(); ?>
+<?php get_footer(); ?>
