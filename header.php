3a44145ac8b9bbd6ca6c74ac5f6b699cca7fe71d
Anders Abel <anders@abel.nu>
2014-01-21 08:21:20 +0100

    Changed to new universal google analytics script.

--- header.php
+++ header.php
@@ -30,6 +30,19 @@
              */
             wp_head();
         ?>
+        <?php if(! is_user_logged_in() ): ?>
+        <!-- Google analytics -->
+        <script>
+          (function(i,s,o,g,r,a,m){i['GoogleAnalyticsObject']=r;i[r]=i[r]||function(){
+          (i[r].q=i[r].q||[]).push(arguments)},i[r].l=1*new Date();a=s.createElement(o),
+          m=s.getElementsByTagName(o)[0];a.async=1;a.src=g;m.parentNode.insertBefore(a,m)
+          })(window,document,'script','//www.google-analytics.com/analytics.js','ga');
+
+          ga('create', 'UA-29041594-1', 'abel.nu');
+          ga('send', 'pageview');
+
+        </script>
+    <?php endif; ?>
     </head>
     <body <?php body_class(); ?>>
         <nav class="navbar navbar-default navbar-fixed-top container navbar-inverse" role="navigation">
