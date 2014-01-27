c1cd8da542bc8dd1ec9df4100fdc2d879292752d
Anders Abel <anders@abel.nu>
2014-01-27 20:55:01 +0100

    Improved error message on 404 page.

--- 404.php
+++ 404.php
@@ -2,11 +2,13 @@
 <div id="primary" class="col-md-8">
     <h1>Who should be embarrassed?</h1>
 
-    <p class="lead">This is somewhat embarrassing. There is no page at this URL. If you got here by clicking 
+    <p class="lead">This is somewhat embarrassing. There is no page at this URL.</p>
+
+    <p> If you got here by clicking
         a link somewhere on the site, then it's embarrasing for me. If you got here by typing an incorrect
         link into the browser, then it's embarrasing for someone close to your keyboard.
     </p>
 
-    </div>
+</div>
 <?php get_sidebar(); ?>
 <?php get_footer(); ?>
